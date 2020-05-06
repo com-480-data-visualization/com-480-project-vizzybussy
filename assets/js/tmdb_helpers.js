347b3625cb52201c7c5cc913f1b21d9e18b8a30c
Jeremy Mion <jeremy.mion@epfl.ch>
2020-05-07 01:29:12 +0200

    image fetch working

--- assets/js/tmdb_helpers.js
+++ assets/js/tmdb_helpers.js
@@ -1,12 +1,31 @@
+//https://github.com/cavestri/themoviedb-javascript-library/wiki/Movies
+
 function setMovieImage(destIDLocation, movieID) {
+    console.log("id : "+movieID)
+    theMovieDb.movies.getById(
+        {"id":movieID },
+        (d) => {
+            console.log(d)
+            var posterPath = JSON.parse(d).poster_path;
+            if(posterPath!=null){
+                $(destIDLocation).attr("src" ,"https://image.tmdb.org/t/p/original" + posterPath)
+            }else{
+                $(destIDLocation).attr("src", "assets/img/black-chairs-in-front-of-white-projector-screen-3709369.jpg")
+            }
+        },
+        (d) => {
+            console.log(d)})
+}
+
+function getMoviePosterPath(movieID){
     theMovieDb.movies.getById(
         {"id":movieID },
         (d) => {
             var posterPath = JSON.parse(d).belongs_to_collection.poster_path;
             if(posterPath!=null){
-                $(destIDLocation).attr("src" ,"https://image.tmdb.org/t/p/w500" + JSON.parse(d).belongs_to_collection.poster_path)
+                return "https://image.tmdb.org/t/p/w500" + JSON.parse(d).belongs_to_collection.poster_path
             }else{
-                $(destIDLocation).attr("src", "black-chairs-in-front-of-white-projector-screen-3709369.jpg")
+               return "assets/img/black-chairs-in-front-of-white-projector-screen-3709369.jpg"
             }
         },
         (d) => {
